2c78f746b9fa2f1f873de055a3ff0404e29a1185
Borivoje Stojiljkovic <stojiljkovicborivoje97@gmail.com>
2023-05-06 11:01:38 +0200

    new

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { CustomerRoutingModule } from './customer-routing.module';
-import { CustomerComponent } from './components/customer/customer.component';
-import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { CustomerComponent } from './components/customer/customer.component';
+import { CustomerRoutingModule } from './customer-routing.module';
 
 
 @NgModule({
